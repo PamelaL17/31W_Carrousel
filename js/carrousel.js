f5f0bc0861a731622db8d20e6e4e7f835a30dd4a
Pamela Limoges <pamelalimoges1@gmail.com>
2024-11-15 13:58:16 -0500

    s5c1 Modification des fichier carrousel.js et style.scss

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,9 +1,10 @@
 (function () {
-  console.log("Vive JavaScript");
+
   let bouton__ouvrir = document.querySelector(".bouton__ouvrir");
   let carrousel__fermer = document.querySelector(".carrousel__fermer");
   let carrousel__x = document.querySelector(".carrousel__x");
   let carrousel = document.querySelector(".carrousel");
+
   bouton__ouvrir.addEventListener("click", function () {
     carrousel.classList.add("carrousel--ouvrir");
     console.log("Ouvrir");
@@ -12,4 +13,5 @@
     carrousel.classList.remove("carrousel--ouvrir");
     console.log("fermer");
   });
+
 })();
