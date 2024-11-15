84de228dc6839ba8a5a903d2b44a6cc4fc9c0232
Pamela Limoges <pamelalimoges1@gmail.com>
2024-11-15 14:19:07 -0500

    Debut des boutons precedant et suivant

--- js/carrousel.js
+++ js/carrousel.js
@@ -7,9 +7,21 @@
   let carrousel__figure = document.querySelector(".carrousel__figure");
   let galerie = document.querySelector(".galerie");
   let galerie__img = document.querySelectorAll(".galerie img");
+  let carrousel__gauche = document.querySelector(".carrousel__gauche");
+  let carrousel__droite = document.querySelector(".carrousel__droite");
   
   console.log(galerie__img.length);
   
+  carrousel__droite.addEventListener("click", function () {
+    index = (index + 1) % galerie__img.length; // passe a l'image suivante et boucle au debut si on depasse la derniere image
+    afficheImage(index);
+  });
+
+  carrousel__gauche.addEventListener("click", function () {
+    index = (index - 1 + galerie__img.length) % galerie__img.length; // passe a l'image precedente et boucle a la fin si on est avant la premiere image
+    afficheImage(index);
+  });
+
   function remplirCarrousel() {
     for (elm of galerie__img) {
       console.log(elm.src);
@@ -18,6 +30,16 @@
       img.classList.add("carrousel__img");
       carrousel__figure.appendChild(img);
     }
+  }
+
+  /**
+   * Creation d'un radio
+   * Cree un element input et ajouter les attribut type, class, value
+   * @param { 
+   * i numero de radio }
+   */
+  function creationRadio(i) {
+    
   }
 
   carrousel__bouton.addEventListener("click", function () {
@@ -37,7 +59,7 @@
   });
 
   function afficheImage(index) {
-    
+
     let carrousel__img = document.querySelectorAll(".carrousel__img");
 
     for (let i = 0; i < carrousel__img.length; i++) {
